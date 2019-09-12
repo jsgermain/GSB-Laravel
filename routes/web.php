6c73785cbf4fd48b3a4e56bad4a23c8370c4d4e0
Johnes Saint Germain <johnbts@outlook.fr>
2019-09-12 20:01:09 +0200

    Ajout les fichiers routes

---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+        /*-------------------- Use case connexion---------------------------*/
+Route::get('/',[
+        'as' => 'chemin_connexion',
+        'uses' => 'connexionController@connecter'
+]);
+
+Route::post('/',[
+        'as'=>'chemin_valider',
+        'uses'=>'connexionController@valider'
+]);
+Route::get('deconnexion',[
+        'as'=>'chemin_deconnexion',
+        'uses'=>'connexionController@deconnecter'
+]);
+
+         /*-------------------- Use case état des frais---------------------------*/
+Route::get('selectionMois',[
+        'as'=>'chemin_selectionMois',
+        'uses'=>'etatFraisController@selectionnerMois'
+]);
+
+Route::post('listeFrais',[
+        'as'=>'chemin_listeFrais',
+        'uses'=>'etatFraisController@voirFrais'
+]);
+
+        /*-------------------- Use case gérer les frais---------------------------*/
+
+Route::get('gererFrais',[
+        'as'=>'chemin_gestionFrais',
+        'uses'=>'gererFraisController@saisirFrais'
+]);
+
+Route::post('sauvegarderFrais',[
+        'as'=>'chemin_sauvegardeFrais', // trouver dans la vue sommaire 
+        'uses'=>'gererFraisController@sauvegarderFrais' // aller dans le controler gérer frais et accèder à la méthode sauvegarde frais 
+]);
+
+
+
